cab6ce5ccad514c0728756b7dfef87d2402df417
渡辺ブルーノ <watanabeburuno@192.168.1.113>
2025-11-10 18:23:34 -0300

    update

--- rust_intro/projects/oop/blog/src/lib.rs
+++ rust_intro/projects/oop/blog/src/lib.rs
@@ -1,40 +1,51 @@
+use std::future::Pending;
+
 pub struct Post {
-    state: Option<Box<State>>,
+    content: String,
+}
+
+pub struct DraftPost {
     content: String,
 }
 
 impl Post {
-    pub fn new() -> Post {
-        Post { 
-            state: Some(Box::new(Draft {})),
+    pub fn new() -> DraftPost {
+        DraftPost { 
             content: String::new(),
         }
     }
 
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
 
-    pub fn content(&self) -> &str {
-        self.state.as_ref().unwrap().content(&self)
-    }
-
-    pub fn request_review(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.request_review())
-        }
-    }
-
-    pub fn approve(&mut self) {
-        if let Some(s) = self.state.take() {
-            self.state = Some(s.approve())
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
         }
     }
 }
 
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post { 
+            content: self.content,
+        }
+    }
+}
 trait State {
-    fn request_review(self: Box<Self>) -> Box<State>;
-    fn approve(self: Box<Self>) -> Box<State>;
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
     fn content<'a>(&self, post: &'a Post) -> &'a str {
         ""
     }
@@ -43,11 +54,11 @@ trait State {
 struct Draft {}
 
 impl State for Draft {
-    fn request_review(self: Box<Self>) -> Box<State> {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
         Box::new(PendingReview{})
     }
 
-    fn approve(self: Box<Self>) -> Box<State> {
+    fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
 }
@@ -55,11 +66,11 @@ impl State for Draft {
 struct PendingReview {}
 
 impl State for PendingReview {
-    fn request_review(self: Box<Self>) -> Box<State> {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
         self
     }
 
-    fn approve(self: Box<Self>) -> Box<State> {
+    fn approve(self: Box<Self>) -> Box<dyn State> {
         Box::new(Published {})
     }
 }
@@ -67,11 +78,11 @@ impl State for PendingReview {
 struct Published {}
 
 impl State for Published {
-    fn request_review(self: Box<Self>) -> Box<State> {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
         self
     }
 
-    fn approve(self: Box<Self>) -> Box<State> {
+    fn approve(self: Box<Self>) -> Box<dyn State> {
         self
     }
 
