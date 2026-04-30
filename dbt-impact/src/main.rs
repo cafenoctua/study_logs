57caf2586ad1cdd8978d2329a3793eccfa9d5310
bruno watanabe <ldlwbru0218@gmail.com>
2026-04-30 19:08:28 +0900

    feat: Implemented bfs

--- dbt-impact/src/main.rs
+++ dbt-impact/src/main.rs
@@ -1,10 +1,19 @@
+mod graph;
 mod manifest;
 use std::path::Path;
 
+use graph::DependencyGraph;
 use manifest::Manifest;
 
 fn main() {
     let path = Path::new("manifest.json");
-    let manifest = Manifest::load(path);
-    print!("{:?}", manifest.unwrap().nodes.len());
+    let manifest = Manifest::load(path).unwrap();
+    print!("{:?}\n", manifest.nodes.len());
+
+    let graph = DependencyGraph::from_manifest(&manifest);
+    let result = graph.downstream("model.dbt_dimensional_modeling.stg_ga_sample", None);
+    println!("{:?}\n", result);
+
+    let result = graph.upstream("model.dbt_dimensional_modeling.fct_visits", None);
+    println!("{:?}\n", result);
 }
